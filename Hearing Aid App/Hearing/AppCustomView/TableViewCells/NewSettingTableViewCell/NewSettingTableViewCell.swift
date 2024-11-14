2f7e6b5bf772d3f45a194cb1b876c341836c6459
Yevhen Zelinskyi <3elinskiy@gmail.com>
2024-11-15 00:49:36 +0200

    HAD-10

---
+++ Hearing Aid App/Hearing/AppCustomView/TableViewCells/NewSettingTableViewCell/NewSettingTableViewCell.swift
@@ -0,0 +1,121 @@
+//
+//  NewSettingTableViewCell.swift
+//  Hearing Aid App
+//
+//  Created by Evgeniy Zelinskiy on 14.11.2024.
+//
+
+import UIKit
+
+enum NewSettingTableViewButtonType: Int {
+    case info
+    case switchButton
+    case rightButton
+    case leftButtonImage
+}
+
+protocol NewSettingTableViewCellDelegate: AnyObject {
+    func didSelectButton(with type: NewSettingTableViewButtonType, from cell: NewSettingTableViewCell)
+}
+
+struct NewSettingTableViewCellModel {
+    var cellId: String?
+    var title: String?
+    var attributedTitle: NSAttributedString? = nil
+    var buttonTypes: [NewSettingTableViewButtonType]
+    var switchState: Bool = false
+    var topInset: CGFloat = 0.0
+    var rightImage: UIImage? = UIImage.init(named: "arrowOpenImage")
+    var rightTintColor: UIColor? = UIColor.appColor(.Purple100)
+    var leftImage: UIImage?
+    weak var delegate: NewSettingTableViewCellDelegate?
+}
+
+typealias NewSettingTableViewCellConfig = АViewCellConfig<NewSettingTableViewCell, NewSettingTableViewCellModel>
+
+final class NewSettingTableViewCell: UITableViewCell, HConfigCellProtocol, UIViewCellNib {
+
+    typealias DataType = NewSettingTableViewCellModel
+    
+    @IBOutlet private weak var leftImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    
+    @IBOutlet private weak var buttonsStackView: UIStackView!
+    
+    @IBOutlet private weak var infoImageView: UIImageView!
+    @IBOutlet private weak var rightImageView: UIImageView!
+    
+    @IBOutlet private weak var mainSwitch: UISwitch!
+    @IBOutlet private weak var mainButton: UIButton!
+    
+    @IBOutlet private weak var separatorView: UIView!
+    
+    @IBOutlet private weak var containerViewTopConstraint: NSLayoutConstraint!
+    
+    private weak var delegate: NewSettingTableViewCellDelegate?
+    
+    // MARK: - Lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+//        NotificationCenter.default.addObserver(self, selector: #selector(didChangeTheme), name: ThemeDidChangeNotificationName, object: nil)
+        
+        infoImageView.image = UIImage.init(systemName: "info.circle")
+        separatorView.backgroundColor = UIColor.appColor(.TableSeparator100)
+        
+        didChangeTheme()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        buttonsStackView.arrangedSubviews.enumerated().forEach { $0.element.isHidden = $0.offset != NewSettingTableViewButtonType.rightButton.rawValue }
+        containerViewTopConstraint.constant = .zero
+        mainButton.isHidden = false
+        leftImageView.isHidden = true
+    }
+    
+    func configure(data: DataType) {
+        delegate = data.delegate
+        if let attributedTitle = data.attributedTitle {
+            titleLabel.attributedText = attributedTitle
+        } else {
+            titleLabel.text = data.title
+        }
+        buttonsStackView.arrangedSubviews.enumerated().forEach { $0.element.isHidden = !data.buttonTypes.contains(NewSettingTableViewButtonType(rawValue: $0.offset)!) }
+        mainSwitch.isOn = data.switchState
+        mainButton.isHidden = !data.buttonTypes.contains(.rightButton)
+        containerViewTopConstraint.constant = data.topInset
+        
+        if data.buttonTypes.contains(.rightButton) {
+            rightImageView.image = data.rightImage
+            rightImageView.tintColor = data.rightTintColor
+        }
+        
+        if data.buttonTypes.contains(.leftButtonImage) {
+            leftImageView.isHidden = false
+            leftImageView.image = data.leftImage
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // MARK: - Private methods
+    @objc private func didChangeTheme() {
+        infoImageView.tintColor = AThemeServicesAp.shared.activeColor
+        mainSwitch.onTintColor = AThemeServicesAp.shared.activeColor
+    }
+    
+    // MARK: - IBActions
+    @IBAction private func buttonAction(_ sender: UIButton) {
+        guard let buttonType = NewSettingTableViewButtonType(rawValue: sender.tag) else {
+            return
+        }
+        delegate?.didSelectButton(with: buttonType, from: self)
+    }
+    
+    @IBAction private func switchValueChanged(_ sender: UISwitch) {
+        delegate?.didSelectButton(with: .switchButton, from: self)
+    }
+}
+
--- Hearing Aid App/Hearing/AppViewsControllers/SettingsFlow/SettingsViewController/ASettingAppViewController.swift
+++ Hearing Aid App/Hearing/AppViewsControllers/SettingsFlow/SettingsViewController/ASettingAppViewController.swift
@@ -1,104 +1,207 @@
 import UIKit
 
+enum SettingAppViewControllerCellId: String {
+    case rateAppCell
+    case shareAppCell
+    case hapticCell
+    case restorePurchasesCell
+    case supportCell
+    case submitCompliantCell
+    case privacyPolicyCell
+    case termsOfUseCell
+}
+
 final class ASettingAppViewController: PMUMainViewController {
 
     @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var topViewHeight: NSLayoutConstraint! // If not premium 232.0, else 0.0
+    @IBOutlet private weak var titleLabel: UILabel!
+    
+    @IBOutlet private weak var premiumBannerView: UIView! // Hide if premium
+    @IBOutlet private weak var premiumBannerTitle: UILabel!
+    @IBOutlet private weak var premiumBannerSubtitle: UILabel!
+    @IBOutlet private weak var premiumBannerButton: UIButton!
     
     private var dataSource: [CellConfigurator] = []
+    private var firstSectionDataSource: [CellConfigurator] = []
+    private var secondSectionDataSource: [CellConfigurator] = []
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(didUpdateSubscriptionInfo), name: TInAppService.didUpdatePurchases, object: nil)
         configureUI()
         configureDataSource()
     }
     
-    override func didChangeTheme() {
-        super.didChangeTheme()
-        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: AThemeServicesAp.shared.activeColor as Any, .font: UIFont.systemFont(ofSize: 28.0, weight: .bold) as Any]
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
+    
+//    override func didChangeTheme() {
+//        super.didChangeTheme()
+//        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: AThemeServicesAp.shared.activeColor as Any, .font: UIFont.systemFont(ofSize: 28.0, weight: .bold) as Any]
+//    }
     
     // MARK: - Private methods
     private func configureUI() {
-        title = "Settings".localized()
-        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: AThemeServicesAp.shared.activeColor as Any, .font: UIFont.systemFont(ofSize: 28.0, weight: .bold) as Any]
+        titleLabel.text = "Settings".localized()
+        premiumBannerTitle.text = "Get Full Access".localized()
+        premiumBannerSubtitle.text = "Unlock Premium to hear with clarity".localized()
+        premiumBannerButton.titleLabel?.text = "Try Free & Subscribe".localized()
         
-        tableView.contentInset = UIEdgeInsets(top: 25.0, left: .zero, bottom: .zero, right: .zero)
+        tableView.contentInset = UIEdgeInsets(top: .zero, left: .zero, bottom: .zero, right: .zero)
         
-        let cellNibs: [UIViewCellNib.Type] = [UThemNTablViewCell.self, SettingTableViewCell.self]
+        let cellNibs: [UIViewCellNib.Type] = [NewSettingTableViewCell.self, NewSettingTableViewEmptyCell.self]
         cellNibs.forEach { tableView.register($0.nib, forCellReuseIdentifier: $0.identifier) }
+        
+        premiumBannerView.isHidden = TInAppService.shared.isPremium
+        topViewHeight.constant = TInAppService.shared.isPremium ? 0.0 : 232.0
+        view.layoutSubviews()
     }
     
     private func configureDataSource() {
-        let themeCellModel = UThemNTablViewCellModel(title: "Color Theme".localized(), selectedTheme: AThemeServicesAp.shared.currentColorType, themes: [.blue, .orange, .red, .green, .purpule], delegate: self)
-        let themeCellConfig = UThemNTablViewCellConfig(item: themeCellModel)
+        let emptyCellModel = NewSettingTableViewEmptyCellModel()
+        let emptyCellConfig = NewSettingTableViewEmptyCellConfig(item: emptyCellModel, height: 15.0)
         
-        let premiumCellModel = SettingTableViewCellModel(title: "Unlock Premium".localized(), buttonTypes: [.rightButton], delegate: self)
-        let premiumCellConfig = SettingTableViewCellConfig(item: premiumCellModel)
+        var rateAppCellModel = NewSettingTableViewCellModel(title: "Rate App".localized(), buttonTypes: [.rightButton, .leftButtonImage], leftImage: UIImage(named: "settingRateImage"), delegate: self)
+        rateAppCellModel.cellId = SettingAppViewControllerCellId.rateAppCell.rawValue
+        let rateAppCellConfig = NewSettingTableViewCellConfig(item: rateAppCellModel, height: 44.0)
         
-        let restoreCellModel = SettingTableViewCellModel(title: "Restore Purchase".localized(), buttonTypes: [.rightButton], delegate: self)
-        let restoreCellConfig = SettingTableViewCellConfig(item: restoreCellModel)
+        var shareAppCellModel = NewSettingTableViewCellModel(title: "Share App".localized(), buttonTypes: [.rightButton, .leftButtonImage], leftImage: UIImage(named: "settingShareImage"), delegate: self)
+        shareAppCellModel.cellId = SettingAppViewControllerCellId.shareAppCell.rawValue
+        let shareAppCellConfig = NewSettingTableViewCellConfig(item: shareAppCellModel, height: 44.0)
         
-        let changeAppIconCellModel = SettingTableViewCellModel(title: "Change App Icon".localized(), buttonTypes: [.rightButton], delegate: self)
-        let changeAppIconCellConfig = SettingTableViewCellConfig(item: changeAppIconCellModel)
+        let betweenSectionCellModel = NewSettingTableViewEmptyCellModel()
+        let betweenSectionCellConfig = NewSettingTableViewEmptyCellConfig(item: betweenSectionCellModel, height: 32.0)
         
-        let hapticCellModel = SettingTableViewCellModel(title: "Use Haptic Feedback".localized(), buttonTypes: [.info, .switchButton], switchState: TapticEngine.isOn, delegate: self)
-        let hapticCellConfig = SettingTableViewCellConfig(item: hapticCellModel)
+        var hapticCellModel = NewSettingTableViewCellModel(title: "Use Haptic Feedback".localized(), buttonTypes: [.info, .switchButton], switchState: TapticEngine.isOn, delegate: self)
+        hapticCellModel.cellId = SettingAppViewControllerCellId.hapticCell.rawValue
+        let hapticCellConfig = NewSettingTableViewCellConfig(item: hapticCellModel, height: 44.0)
         
-        let faqCellModel = SettingTableViewCellModel(title: "FAQ".localized(), buttonTypes: [.rightButton], delegate: self)
-        let faqCellConfig = SettingTableViewCellConfig(item: faqCellModel)
+        var restorePurchasesCellModel = NewSettingTableViewCellModel(title: "Restore Purchases".localized(), buttonTypes: [.rightButton], delegate: self)
+        restorePurchasesCellModel.cellId = SettingAppViewControllerCellId.restorePurchasesCell.rawValue
+        let restorePurchasesCellConfig = NewSettingTableViewCellConfig(item: restorePurchasesCellModel, height: 44.0)
         
-        let websiteCellModel = SettingTableViewCellModel(title: "Our website".localized(), buttonTypes: [.rightButton], delegate: self)
-        let websiteCellConfig = SettingTableViewCellConfig(item: websiteCellModel)
+        var supportCellModel = NewSettingTableViewCellModel(title: "Support & Contact Us".localized(), buttonTypes: [.rightButton], delegate: self)
+        supportCellModel.cellId = SettingAppViewControllerCellId.supportCell.rawValue
+        let supportCellConfig = NewSettingTableViewCellConfig(item: supportCellModel, height: 44.0)
+        supportCellConfig.height = 44.0
         
-        let moreCellModel = SettingTableViewCellModel(title: "More".localized(), buttonTypes: [.rightButton], delegate: self)
-        let moreCellConfig = SettingTableViewCellConfig(item: moreCellModel)
+        var submitCompliantCellModel = NewSettingTableViewCellModel(title: "Submit a Compliant".localized(), buttonTypes: [.rightButton], delegate: self)
+        submitCompliantCellModel.cellId = SettingAppViewControllerCellId.submitCompliantCell.rawValue
+        let submitCompliantCellConfig = NewSettingTableViewCellConfig(item: submitCompliantCellModel, height: 44.0)
         
-        let supportCellModel = SettingTableViewCellModel(title: "Support & Contact Us".localized(), buttonTypes: [.rightButton], delegate: self)
-        let supportCellConfig = SettingTableViewCellConfig(item: supportCellModel)
+        var privacyPolicyCellModel = NewSettingTableViewCellModel(title: "Privacy Policy".localized(), buttonTypes: [.rightButton], delegate: self)
+        privacyPolicyCellModel.cellId = SettingAppViewControllerCellId.privacyPolicyCell.rawValue
+        let privacyPolicyCellConfig = NewSettingTableViewCellConfig(item: privacyPolicyCellModel, height: 44.0)
         
-        dataSource = [themeCellConfig, premiumCellConfig, restoreCellConfig, changeAppIconCellConfig, hapticCellConfig, faqCellConfig, websiteCellConfig, moreCellConfig, supportCellConfig]
+        var termsOfUseCellModel = NewSettingTableViewCellModel(title: "Terms of Use".localized(), buttonTypes: [.rightButton], delegate: self)
+        termsOfUseCellModel.cellId = SettingAppViewControllerCellId.termsOfUseCell.rawValue
+        let termsOfUseCellConfig = NewSettingTableViewCellConfig(item: termsOfUseCellModel, height: 44.0)
+        
+        firstSectionDataSource = [rateAppCellConfig, emptyCellConfig, shareAppCellConfig, betweenSectionCellConfig]
+        secondSectionDataSource = [hapticCellConfig, emptyCellConfig, restorePurchasesCellConfig, emptyCellConfig, supportCellConfig, emptyCellConfig, submitCompliantCellConfig, emptyCellConfig, privacyPolicyCellConfig, emptyCellConfig, termsOfUseCellConfig]
+        dataSource = firstSectionDataSource + secondSectionDataSource
         tableView.reloadData()
+    }
+    
+    // MARK: - Action
+    @IBAction func onPremiumBannerTap(_ sender: UITapGestureRecognizer) {
+        AppsNavManager.shared.presentPaywallViewController(with: .openFromSetting)
+    }
+    
+    @objc func didUpdateSubscriptionInfo() {
+        premiumBannerView.isHidden = TInAppService.shared.isPremium
+        topViewHeight.constant = TInAppService.shared.isPremium ? 0.0 : 232.0
+        view.layoutSubviews()
     }
 }
 
 // MARK: - UITableViewDataSource, UITableViewDelegate
 extension ASettingAppViewController: UITableViewDataSource, UITableViewDelegate {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSource.count
+        return section == 0 ? firstSectionDataSource.count : secondSectionDataSource.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellConfig: CellConfigurator = dataSource[indexPath.row]
+        var cellConfig: CellConfigurator
+        if indexPath.section == 0 {
+            cellConfig = firstSectionDataSource[indexPath.row]
+        } else {
+            cellConfig = secondSectionDataSource[indexPath.row]
+            
+        }
         let cell = tableView.dequeueReusableCell(withIdentifier: cellConfig.identifier, for: indexPath)
         cellConfig.configure(cell: cell)
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return dataSource[indexPath.row].height ?? UITableView.automaticDimension
+        let heightForRow: CGFloat?
+        if indexPath.section == 0 {
+            heightForRow = firstSectionDataSource[indexPath.row].height
+        } else {
+            heightForRow = secondSectionDataSource[indexPath.row].height
+        }
+        return heightForRow ?? UITableView.automaticDimension
     }
     
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return dataSource[indexPath.row].height ?? UITableView.automaticDimension
+        let estimatedHeightForRow: CGFloat?
+        if indexPath.section == 0 {
+            estimatedHeightForRow = firstSectionDataSource[indexPath.row].height
+        } else {
+            estimatedHeightForRow = secondSectionDataSource[indexPath.row].height
+        }
+        return estimatedHeightForRow ?? UITableView.automaticDimension
     }
 }
 
-// MARK: - SettingTableViewCellDelegate
-extension ASettingAppViewController: SettingTableViewCellDelegate {
+// MARK: - NewSettingTableViewCellDelegate
+extension ASettingAppViewController: NewSettingTableViewCellDelegate {
     
-    func didSelectButton(with type: SettingTableViewButtonType, from cell: SettingTableViewCell) {
-        guard let indexRow = tableView.indexPath(for: cell)?.row,
-              let cellModel = dataSource[indexRow].getItem() as? SettingTableViewCellModel else {
+    func didSelectButton(with type: NewSettingTableViewButtonType, from cell: NewSettingTableViewCell) {
+        guard let indexPath = tableView.indexPath(for: cell) else {
+            return
+        }
+        let itemModel = indexPath.section == 0 ? firstSectionDataSource[indexPath.row].getItem() : secondSectionDataSource[indexPath.row].getItem()
+        guard let cellModel = itemModel as? NewSettingTableViewCellModel, let cellIdRawValue = cellModel.cellId else {
             return
         }
         TapticEngine.impact.feedback(.medium)
         
-        switch indexRow {
-        case 1: // Premium
-            AppsNavManager.shared.presentPaywallViewController(with: .openFromSetting)
-        case 2: // Restore
+        let cellId = SettingAppViewControllerCellId(rawValue: cellIdRawValue)
+        switch cellId {
+        case .rateAppCell: // Rate app
+            KAppConfigServic.shared.settings.presentAppRatingAlert()
+        case .shareAppCell: // Share app
+            AppsNavManager.shared.presentShareViewController(with: [CAppConstants.URLs.appStoreUrl], and: nil)
+        case .hapticCell: // Haptic
+            switch type {
+            case .info:
+                presentAlertPM(title: "Info".localized(), message: "Disable or Enable vibrations when you press UI buttons".localized())
+                
+                KAppConfigServic.shared.analytics.track(action: .v2SettingsScreen, with: [GAppAnalyticActions.action.rawValue: "\(GAppAnalyticActions.haptic.rawValue)_\(GAppAnalyticActions.info.rawValue)"])
+            case .switchButton:
+                let newState = !TapticEngine.isOn
+                TapticEngine.isOn = newState
+                //ThemeSettingsRow.TapticOption.setValue(newState)
+
+                let newCellModel = NewSettingTableViewCellModel(title: cellModel.title, buttonTypes: cellModel.buttonTypes, switchState: newState, topInset: cellModel.topInset, delegate: self)
+                secondSectionDataSource[indexPath.row] = NewSettingTableViewCellConfig(item: newCellModel)
+
+                let stringState = newState ? GAppAnalyticActions.enable.rawValue : GAppAnalyticActions.disable.rawValue
+                KAppConfigServic.shared.analytics.track(action: .v2SettingsScreen, with: [GAppAnalyticActions.action.rawValue: "\(GAppAnalyticActions.haptic.rawValue)_\(stringState)"])
+            default:
+                break
+            }
+        case .restorePurchasesCell: // Restore
             HAppLoaderView.showLoader(at: AppsNavManager.shared.topViewController?.view ?? view, animated: true)
             TInAppService.shared.restorePurchases { [weak self] isSuccess in
                 guard let self = self else {
@@ -111,68 +214,19 @@ extension ASettingAppViewController: SettingTableViewCellDelegate {
             }
             
             KAppConfigServic.shared.analytics.track(action: .v2SettingsScreen, with: [GAppAnalyticActions.action.rawValue: GAppAnalyticActions.restore.rawValue])
-        case 3: // App icon
-            AppsNavManager.shared.presentZAppIconViewController()
-            
-            KAppConfigServic.shared.analytics.track(.v2SettingsScreen, with: [GAppAnalyticActions.action.rawValue: GAppAnalyticActions.changeAppIcon.rawValue])
-        case 4: // Haptic
-            switch type {
-            case .info:
-                presentAlertPM(title: "Info".localized(), message: "Disable or Enable vibrations when you press UI buttons".localized())
-                
-                KAppConfigServic.shared.analytics.track(action: .v2SettingsScreen, with: [GAppAnalyticActions.action.rawValue: "\(GAppAnalyticActions.haptic.rawValue)_\(GAppAnalyticActions.info.rawValue)"])
-            case .switchButton:
-                let newState = !TapticEngine.isOn
-                TapticEngine.isOn = newState
-                //ThemeSettingsRow.TapticOption.setValue(newState)
-
-                let newCellModel = SettingTableViewCellModel(title: cellModel.title, buttonTypes: cellModel.buttonTypes, switchState: newState, topInset: cellModel.topInset, delegate: self)
-                dataSource[indexRow] = SettingTableViewCellConfig(item: newCellModel)
-
-                let stringState = newState ? GAppAnalyticActions.enable.rawValue : GAppAnalyticActions.disable.rawValue
-                KAppConfigServic.shared.analytics.track(action: .v2SettingsScreen, with: [GAppAnalyticActions.action.rawValue: "\(GAppAnalyticActions.haptic.rawValue)_\(stringState)"])
-            default:
-                break
-            }
-        case 5: // FAQ
-            AppsNavManager.shared.presentSafariViewController(with: CAppConstants.URLs.faqURL)
-            
-            KAppConfigServic.shared.analytics.track(.v2SettingsScreen, with: [GAppAnalyticActions.action.rawValue: GAppAnalyticActions.faq.rawValue])
-        case 6: // Website
-            AppsNavManager.shared.presentSafariViewController(with: CAppConstants.URLs.ourWebSiteURL)
-            
-            KAppConfigServic.shared.analytics.track(.v2SettingsScreen, with: [GAppAnalyticActions.action.rawValue: GAppAnalyticActions.ourWebsite.rawValue])
-        case 7: // More
-            AppsNavManager.shared.presentOMoreApViewController()
-            
-            KAppConfigServic.shared.analytics.track(action: .v2SettingsScreen, with: [GAppAnalyticActions.action.rawValue: GAppAnalyticActions.more.rawValue])
-        case 8: // Support
+        case .supportCell: // Support
             AppsNavManager.shared.presentSupportViewController()
             
             KAppConfigServic.shared.analytics.track(action: .v2SettingsScreen, with: [GAppAnalyticActions.action.rawValue: GAppAnalyticActions.support.rawValue])
+        case .submitCompliantCell: // Submit Compliant
+            AppsNavManager.shared.presentSupportViewController()
+        case .privacyPolicyCell: // Privacy Policy
+            AppsNavManager.shared.presentSafariViewController(with: CAppConstants.URLs.privacyPolicyURL)
+        case .termsOfUseCell: // Terms of use
+            AppsNavManager.shared.presentSafariViewController(with: CAppConstants.URLs.termsURL)
         default:
             break
         }
-    }
-}
-
-// MARK:  UThemNTablViewCellDelegate
-extension ASettingAppViewController: UThemNTablViewCellDelegate {
-    
-    func didSelectTheme(with type: ColorType, from cell: UThemNTablViewCell) {
-        guard let indexRow = tableView.indexPath(for: cell)?.row,
-              let cellModel = dataSource[indexRow].getItem() as? UThemNTablViewCellModel else {
-            return
-        }
-        
-        TapticEngine.impact.feedback(.medium)
-        AThemeServicesAp.shared.setColorType(type)
-        KAppConfigServic.shared.settings.presentAppRatingAlert()
-        
-        let newCellModel = UThemNTablViewCellModel(title: cellModel.title, selectedTheme: AThemeServicesAp.shared.currentColorType, themes: cellModel.themes, delegate: self)
-        dataSource[indexRow] = UThemNTablViewCellConfig(item: newCellModel)
-        
-        KAppConfigServic.shared.analytics.track(action: .v2SettingsScreen, with: [GAppAnalyticActions.action.rawValue: "\(GAppAnalyticActions.select.rawValue)_\(type.analyticAction.rawValue)"])
     }
 }
 
