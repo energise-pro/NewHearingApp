59471bef357f2891f458690c43ba0fdd3e4bcc64
admin <admin@gmail.com>
2024-11-14 15:39:57 +0200

    fix black theme

--- Hearing Aid App/AppDelegate.swift
+++ Hearing Aid App/AppDelegate.swift
@@ -6,6 +6,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if #available(iOS 13.0, *) {
+                window?.overrideUserInterfaceStyle = .light
+        }
         KAppConfigServic.shared.app(application, didFinishLaunch: launchOptions)
         return true
     }
