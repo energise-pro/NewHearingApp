c8cea716b4ac0fce81512fb50717a83e6af5d9b1
Yevhen Zelinskyi <3elinskiy@gmail.com>
2024-12-12 12:41:00 +0200

    HAD-44

--- Hearing Aid App/Hearing/AppViewsControllers/RemindersFlow/MicrophonePermissionViewController/HMicrophPermisApViewController.swift
+++ Hearing Aid App/Hearing/AppViewsControllers/RemindersFlow/MicrophonePermissionViewController/HMicrophPermisApViewController.swift
@@ -33,32 +33,12 @@ final class HMicrophPermisApViewController: UIViewController {
     
     //MARK: - Private methods
     private func configureUI() {
-        titleLabel.textColor = UIColor.appColor(.UnactiveButton_1)
-        descriptionLabel.textColor = UIColor.appColor(.UnactiveButton_1)?.withAlphaComponent(0.8)
-        allowButtonLabel.textColor = .white
+        titleLabel.textColor = UIColor.appColor(.White100)
+        descriptionLabel.textColor = UIColor.appColor(.White100)
+        allowButtonLabel.textColor = UIColor.appColor(.Purple100)
         
-        titleLabel.text = "Please Allow Microphone Usage".localized()
-        descriptionLabel.text = "Dear User, for correctly work with our app, we need your microphone usage permission".localized()
-        allowButtonLabel.text = "Continue".localized()
-        
-        allowButtonContainerView.backgroundColor = AThemeServicesAp.shared.activeColor
-        
-        let fontMultiplier: CGFloat = 0.03
-        let titleFontSize = min(max(.appHeight * fontMultiplier, Defaults.Sizes.minimumTitleFontSize), Defaults.Sizes.maximumTitleFontSize)
-        let descriptionFontSize = min(max(.appHeight * fontMultiplier, Defaults.Sizes.minimumDescriptionFontSize), Defaults.Sizes.maximumDescriptionFontSize)
-        
-        titleLabel.font = UIFont.systemFont(ofSize: titleFontSize, weight: .bold)
-        descriptionLabel.font = UIFont.systemFont(ofSize: descriptionFontSize, weight: .medium)
-    }
-}
-
-private struct Defaults {
-    
-    struct Sizes {
-        static let minimumTitleFontSize: CGFloat = 22.0
-        static let maximumTitleFontSize: CGFloat = 28.0
-        
-        static let minimumDescriptionFontSize: CGFloat = 16.0
-        static let maximumDescriptionFontSize: CGFloat = 25.0
+        titleLabel.text = "Allow Microphone Usage".localized()
+        descriptionLabel.text = "Please allow microphone usage for sound recognition and hearing aid.".localized()
+        allowButtonLabel.text = "Go to Settings".localized()
     }
 }
