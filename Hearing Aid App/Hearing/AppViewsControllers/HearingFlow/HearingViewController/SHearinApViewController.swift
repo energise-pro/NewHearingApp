a287efb49d169dc7a60e115fa1b5185dd82d5c43
Yevhen Zelinskyi <3elinskiy@gmail.com>
2025-01-08 09:58:18 +0200

    HAD-64

--- Hearing Aid App/Hearing/AppViewsControllers/HearingFlow/HearingViewController/SHearinApViewController.swift
+++ Hearing Aid App/Hearing/AppViewsControllers/HearingFlow/HearingViewController/SHearinApViewController.swift
@@ -362,7 +362,7 @@ final class SHearinApViewController: PMUMainViewController {
     }
     
     private func updateHeadphonesTitleLabel() {
-        headphonesTitleLabel.text = SAudioKitServicesAp.shared.connectedHeadphones ? (!SAudioKitServicesAp.shared.outputDeviceName.isEmpty ? SAudioKitServicesAp.shared.outputDeviceName : "Connect Headphones".localized()) : "Connect Headphones".localized()
+        headphonesTitleLabel.text = SAudioKitServicesAp.shared.connectedHeadphones ? SAudioKitServicesAp.shared.outputDeviceName : "Connect Headphones".localized()
     }
     
     @objc private func audioRouteChanged(notification: NSNotification) {
